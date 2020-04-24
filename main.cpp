77402aaf7c666d6abd7c23d2ca3df04b9ffe0e91
Ilia <fortyhell@gmail.com>
2020-04-24 18:02:25 +0300

    Add class magma in header file

--- main.cpp
+++ main.cpp
@@ -7,7 +7,18 @@ public:
     Gamma();
     ~Gamma();
 };
+int Magma::mode = 0;
 
 int main() {
+    Magma::setDecrypt();
+    cout << hex;
+    int256 key(0xffeeddccbbaa9988, 0x7766554433221100, 0xf0f1f2f3f4f5f6f7, 0xf8f9fafbfcfdfeff);
+    Magma a(key);
+    ullong text = 0xfedcba9876543210;
+    ullong message = a.encrypt(text);
+    cout << "\nciphertext = " << message << endl;
+    message = a.decrypt(message);
+    cout << "\nmessage = " << message;
+    cout << "\noriginal = " << text;
     return 0;
 }
