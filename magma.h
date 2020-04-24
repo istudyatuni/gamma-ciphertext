ae90f250edbc636fde5408544e32ec14096a18cb
Ilia <fortyhell@gmail.com>
2020-04-24 16:41:31 +0300

    Initial commit

---
+++ magma.h
@@ -0,0 +1,6 @@
+#ifndef MAGMA_H
+#define MAGMA_H
+
+
+
+#endif // MAGMA_H
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "magma.h"
+using namespace std;
+
+class Gamma {
+public:
+    Gamma();
+    ~Gamma();
+};
+
+int main() {
+    return 0;
+}
